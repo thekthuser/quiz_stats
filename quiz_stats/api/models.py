617b53454ff415eb4afac0c006985e58257fa1f0
thekthuser <thekthuser@thekthuser.com>
2019-03-11 11:36:44 -0400

    created Proejct model

--- quiz_stats/api/models.py
+++ quiz_stats/api/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+  name = models.CharField(max_length=127, default='Untitled Project')
+  description = models.CharField(max_length=255, default='')
+  is_active = models.BooleanField(default=True)
+  created = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.name

469830e9c0b74d593647bc6cf442f30db9bde149
thekthuser <thekthuser@thekthuser.com>
2019-03-12 13:39:11 -0400

    create 'web' app and link to index TemplateView.as_view

--- quiz_stats/quiz_stats/urls.py
+++ quiz_stats/quiz_stats/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 from api.resources import ProjectResource, TokenResource, QuestionResource, RelationshipResource, AnswerResource
 
 project_resource = ProjectResource()
@@ -30,4 +31,5 @@ urlpatterns = [
     url(r'^api/', include(question_resource.urls)),
     url(r'^api/', include(relationship_resource.urls)),
     url(r'^api/', include(answer_resource.urls)),
+    url(r'/', TemplateView.as_view(template_name='index.html')),
 ]

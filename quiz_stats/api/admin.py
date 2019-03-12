ab44edc3f8983eaece828cee0730a32a4ac53caa
thekthuser <thekthuser@thekthuser.com>
2019-03-11 21:32:02 -0400

    register models for admin dashboard

--- quiz_stats/api/admin.py
+++ quiz_stats/api/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Project)
+admin.site.register(models.Token)
+admin.site.register(models.Question)
+admin.site.register(models.Relationship)
+admin.site.register(models.Answer)
